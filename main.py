8c232f4a49c0d0bf11ed29cb9b3bed0aff5cb6b8
yongteck <160562805+yongteck@users.noreply.github.com>
2024-03-01 02:56:46 +0000

    v3 submission

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-num = input('Enter a number (decimal or integer): ').split()[0]
+num = input('Enter a number (decimal or integer): ').strip(" ").lstrip("0")
 # type your code here
-sf = 0
-nums = "123456789"
-place = 0
-while num[place] not in nums: #looks past the leading 0s
-  place += 1
-if float(num) < 1:
-    sf = len(num) - place
-else:
-    sf = len(num) - 1 - place #12.01 sf is 4
-
+sf = len(num.replace(".",""))
 # do not change any code beyond this point
 print('The number', num, 'has', sf, 'significant figures.')
