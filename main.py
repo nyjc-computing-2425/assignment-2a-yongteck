db4bc9230ebec04f82d929ac4eefe22c4b1c8c31
yongteck <160562805+yongteck@users.noreply.github.com>
2024-03-01 01:47:34 +0000

    v1 submission

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-num = input('Enter a number (decimal or integer): ')
+num =input('Enter a number (decimal or integer): ')
 # type your code here
-
-
+sf = 0
+nums = "123456789"
+place = 0
+if float(num) < 1:
+    while num[place] not in nums: #looks past the 0.00 in 0.007
+        place += 1
+    sf = len(num) - place
+else:
+    sf = len(num) - 1 #12.01 sf is 4
 
 # do not change any code beyond this point
 print('The number', num, 'has', sf, 'significant figures.')
