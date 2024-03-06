dc46b2615d45def55448f014777d389094126a06
yongteck <160562805+yongteck@users.noreply.github.com>
2024-03-06 06:46:04 +0000

    version final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-num = input('Enter a number (decimal or integer): ').strip(" ").lstrip("0")
+num = input('Enter a number (decimal or integer): ').strip(" ")
+num1 = num.replace(".","").lstrip("0")
 # type your code here
-sf = len(num.replace(".",""))
+sf = len(num1)
 # do not change any code beyond this point
 print('The number', num, 'has', sf, 'significant figures.')
